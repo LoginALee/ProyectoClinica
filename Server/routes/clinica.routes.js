6bbdfb675b0036c576d18f6f72b5355c935f4835
LoginALee <alejandrodxwwe@hotmail.com>
2019-05-27 18:51:31 -0500

    Commit

--- Server/routes/clinica.routes.js
+++ Server/routes/clinica.routes.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const persona = require('../Controllers/persona.controller');
+const users = require('../Controllers/auth.controller');
+
 
 router.get('/', persona.getPersonas);
 router.post('/', persona.createPersona);
 router.get('/:id', persona.getPersonaUnica);
 router.put('/:id', persona.editPersona);
 router.delete('/:id', persona.deletePersona);
+
 
 
 
